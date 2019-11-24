e0c767d6c2685f03179deeba1002193755a14902
Anand <50521705+arockiaanandraj@users.noreply.github.com>
2019-11-24 20:17:31 +0530

    Initial Commit

---
+++ src/test/java/com/retail/services/customerservice/CustomerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.retail.services.customerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
