2a43cfcd7b624e2c143311313c1a8057f844ecea
arockiaanandraj <arockiaanandraj@gmail.com>
2019-11-30 03:36:21 +0530

    Added JUnit tests

--- src/main/java/com/retail/services/customerservice/model/Customer.java
+++ src/main/java/com/retail/services/customerservice/model/Customer.java
@@ -2,12 +2,15 @@ package com.retail.services.customerservice.model;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
 public class Customer {
 
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "id")
 	private Long id;
 
--- src/test/java/com/retail/services/customerservice/CustomerServiceApplicationTests.java
+++ src/test/java/com/retail/services/customerservice/CustomerServiceApplicationTests.java
@@ -1,13 +1,68 @@
 package com.retail.services.customerservice;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-@SpringBootTest
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.ActiveProfiles;
+
+import com.retail.services.customerservice.model.Customer;
+
+@ActiveProfiles("junit")
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 class CustomerServiceApplicationTests {
 
+	private static String URL_PREFIX = "http://localhost:";
+
+	@LocalServerPort
+	private int port;
+
+	@Autowired
+	TestRestTemplate testRestTemplate;
+
 	@Test
-	void contextLoads() {
+	void testGetAllCustomers() {
+
+		ResponseEntity<List<Customer>> response = testRestTemplate.exchange(URL_PREFIX + port + "/service1/customers",
+				HttpMethod.GET, null, new ParameterizedTypeReference<List<Customer>>() {
+				});
+		List<Customer> customers = response.getBody();
+		assertTrue(customers.size() > 0);
+		assertEquals("John", customers.get(0).getFirstName());
+
+	}
+
+	@Test
+	void testAddCustomer() {
+
+		Customer customer = new Customer();
+		customer.setFirstName("John");
+		customer.setLastName("Smith");
+		ResponseEntity<String> response = testRestTemplate.postForEntity(URL_PREFIX + port + "/service1/customer",
+				customer, String.class);
+		String responseMessage = response.getBody();
+		assertTrue(responseMessage.startsWith("Added"));
+
+	}
+
+	@Test
+	void testToShowfaultTolerance() {
+
+		ResponseEntity<String> response = testRestTemplate.getForEntity(URL_PREFIX + port + "/service1/fault",
+				String.class);
+		String responseMessage = response.getBody();
+		assertEquals("Fallback method has been executed successfully.", responseMessage);
+
 	}
 
 }
